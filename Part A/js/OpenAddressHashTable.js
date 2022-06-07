38d873c13beb34d5dffffbaca709c4bca85e929e
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-06-07 18:46:46 -0400

    Add putvalue , replace & rehash functions

--- Part A/js/OpenAddressHashTable.js
+++ Part A/js/OpenAddressHashTable.js
@@ -52,12 +52,12 @@ export default class OpenAddressHashTable {
         // index to length
         for(let i=0; i<this.length; i++){
             pair=this.hashTable[i];
-            if(pair.key===key) return pair.value;
+            if(pair!=null && pair.key===key) return pair.value;
         }
         /// 0 to index
         for(let i=0; i<index; i++){
             pair=this.hashTable[i];
-            if(pair.key===key) return pair.value;
+            if(pair!=null && pair.key===key) return pair.value;
         }
 
         return null;
@@ -69,10 +69,85 @@ export default class OpenAddressHashTable {
 
     // @todo - YOU MUST DEFINE THIS METHOD
     putValue(key, item) {
-        
+        /// Declaring Variables 
+        let index = this.hashCode(key);
+        let pair = this.hashTable[index];
+        let NewPair = new KeyValuePair(key,item);
 
+        //// Case 1 Key Exists in the HashTable 
+        if(this.replace(key,item)) return;
+
+        /// Case 2 HashTable is Full
+         if(this.size===this.length){
+            this.length=this.length*2;
+            this.rehash();
+            index = this.hashCode(key);
+            pair  = this.hashTable[index];
+         }
+        
+        /// Case 3 Insert Item Into Natural Index If Available If Not Linear Probing
+        /// Natural Index
+        if(pair==null){
+            this.hashTable[index]=NewPair;
+            this.size++;
+            return;
+        }
+        /// Linear Probing 
+
+        // index to length
+        for(let i=index; i<this.length; i++){
+            pair=this.hashTable[i];
+            if(pair==null) {
+                this.hashTable[i]=NewPair;
+                this.size++;
+                return;
+            }
+        }
+        
+        /// 0 to index
+        for(let i=0; i<index; i++){
+            pair=this.hashTable[i];
+            if(pair==null) {
+                this.hashTable[i]=NewPair;
+                this.size++;
+                return;
+            }
+        }
     }
-    
+
+    replace(key,item){
+        let index = this.hashCode(key);
+        let pair  = this.hashTable[index];
+
+        for(let i=index; i<this.length; i++){
+            pair=this.hashTable[i];
+            if(pair!=null && pair.key===key){
+                pair.value=item;
+                return true;
+            } 
+        }
+        for(let i=0; i<index; i++){
+            pair=this.hashTable[i];
+            if(pair!=null && pair.key===key){
+                pair.value=item;
+                return true;
+            } 
+        }
+        return false;
+    }
+
+    rehash(){
+        let values = Object.values(this.hashTable);
+        console.log(values);
+        for(let i=0; i<this.length; i++){
+            this.hashTable[i]=null;
+        }
+        for(let i=0; i<values.length; i++){
+         this.putValue(values[i].key,values[i].value);
+        }
+    }
+
+
     toString() {
         let text = "[\n";
         for (let i = 0; i < this.length; i++) {
--- Part A/js/Test.js
+++ Part A/js/Test.js
@@ -31,34 +31,34 @@ addPersonToHashTable(new Student(hashTable.generateKey(), "Jimi", "Hendrix", 3.6
 addPersonToHashTable(new Person(hashTable.generateKey(), "Roger", "Waters"), hashTable);
 
 // DEMONSTRATE MAKING KEYS AND ADDING VALUES TO THE HASH TABLE    
-let jlKey = hashTable.generateKey();
-hashTable.putValue(jlKey, new Student(jlKey, "John", "Lennon", 3.8));
-let cwKey = hashTable.generateKey();
-hashTable.putValue(cwKey, new Student(cwKey, "Charlie", "Watts", 3.1));
-let dgKey = hashTable.generateKey();
-hashTable.putValue(dgKey, new Employee(dgKey, "David", "Gilmour", 120000));
-printHashTable("\nAfter Changing 3 Items", hashTable);
+    let jlKey = hashTable.generateKey();
+    hashTable.putValue(jlKey, new Student(jlKey, "John", "Lennon", 3.8));
+    let cwKey = hashTable.generateKey();
+    hashTable.putValue(cwKey, new Student(cwKey, "Charlie", "Watts", 3.1));
+    let dgKey = hashTable.generateKey();
+    hashTable.putValue(dgKey, new Employee(dgKey, "David", "Gilmour", 120000));
+    printHashTable("\nAfter Changing 3 Items", hashTable);
 
 // DEMONSTRATE GETTING VALUES FROM THE HASH TABLE
-let p = hashTable.getValue(jlKey);
-console.log("\nget " + jlKey + ": " + p.toString() + "\n");
-p = hashTable.getValue(cwKey);
-console.log("\nget " + cwKey + ": " + p.toString() + "\n");
-p = hashTable.getValue(dgKey);
-console.log("\nget " + dgKey + ": " + p.toString() + "\n");
+    let p = hashTable.getValue(jlKey);
+    console.log("\nget " + jlKey + ": " + p.toString() + "\n");
+    p = hashTable.getValue(cwKey);
+    console.log("\nget " + cwKey + ": " + p.toString() + "\n");
+    p = hashTable.getValue(dgKey);
+    console.log("\nget " + dgKey + ": " + p.toString() + "\n");
 
 // NOW LET'S TRY REPLACING THE DATA IN THE ABOVE THREE
-hashTable.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
-hashTable.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
-hashTable.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
-printHashTable("\nAfter Changing 3 Items", hashTable);
+    hashTable.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
+    hashTable.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
+    hashTable.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
+    printHashTable("\nAfter Changing 3 Items", hashTable);
 
 // AND DEMONSTRATE REMOVING ITEMS FROM THE BST
-hashTable.removeValue(jlKey);
-printHashTable("\nAfter Removing Otis Redding", hashTable);
+// hashTable.removeValue(jlKey);
+// printHashTable("\nAfter Removing Otis Redding", hashTable);
 
-hashTable.removeValue(cwKey);
-printHashTable("\nAfter Removing Keith Richards", hashTable);
+// hashTable.removeValue(cwKey);
+// printHashTable("\nAfter Removing Keith Richards", hashTable);
 
-hashTable.removeValue(dgKey);
-printHashTable("\nAfter Removing Bill Withers", hashTable);
+// hashTable.removeValue(dgKey);
+// printHashTable("\nAfter Removing Bill Withers", hashTable);
